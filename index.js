21b6a0450cfa2fb114a3246194113d5bb563adc7
karenhernandez <khernandez0329@gmail.com>
2023-10-31 23:02:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(){
+    console.log("function exists")
+
+}
+function saturdayFun(activity="roller-skate"){
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(){
+    console.log("function exists")
+}
+function mondayWork(place="go to the office"){
+    return (`This Monday, I will ${place}.`)
+}
+
+function wrapAdjective(){
+    console.log('fuction exists')
+}
+
+function wrapAdjective(special){
+   return function (string){
+    return `You are ${special}${string}${special}!`
+   }
+   
+
+}
